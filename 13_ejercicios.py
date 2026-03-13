f05ab05c1df4450bc7a78ad8abdff7f2c2136aad
valeriabastidas-coder <valeriabastidascancino@gmail.com>
2026-03-12 22:27:14 -0500

    Delete age categorization from 13_ejercicios.py
    Removed age categorization logic for input processing.

--- 13_ejercicios.py
+++ 13_ejercicios.py
@@ -24,20 +24,3 @@ while True:
 
 print("Total del día:", total_dia)
 
-14. capacidad = int(input("Capacidad sala: "))
-
-niños = adultos = mayores = 0
-
-for i in range(capacidad):
-    edad = int(input("Edad: "))
-
-    if edad < 18:
-        niños += 1
-    elif edad < 60:
-        adultos += 1
-    else:
-        mayores += 1
-
-print("Niños:", niños)
-print("Adultos:", adultos)
-print("Adultos mayores:", mayores)
